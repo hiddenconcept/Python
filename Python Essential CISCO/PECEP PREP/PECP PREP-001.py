6911aaa7d27e9a99089182a77f318462f7193d33
Myles Patricio <myles.patricio@gmail.com>
2026-07-09 08:26:15 -0400

    Add files via upload

---
+++ Python Essential CISCO/PECEP PREP/PECP PREP-001.py
@@ -0,0 +1,45 @@
+# Notes for the PCEP Entry Exam as I am going through the CISCO Python course.
+
+#Data Types print accepts
+#Text
+print("Python")
+
+#Interger
+# Any whole number written without a decimal point is automatically treated as an integer data type,
+# which Python represents internally as the <class 'int'> type.
+print(42)
+
+#Float
+# In Python, any number that contains a decimal point is automatically classified as a floating-point number
+print(3.14)
+print(True)
+print(False)
+
+#Expresions (Mathimatical equations)
+
+#Adding
+print(5 + 2)
+M#ultiplication
+print(5 * 2)
+#Division
+print(5 / 2)
+
+print(5 // 2)
+print(5 % 2)
+print(5 ** 2)
+
+print()
+#Strings vs number
+print("Stringers vs numbers:")
+
+print("5" + "2")
+# 52
+print(5 + 2)
+# 7
+#Escape characters
+
+#n
+#\t
+#\\
+#\"
+#\'
