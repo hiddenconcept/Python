0d88d7b73d2b94ed72870ea39fb10a0eec0abfba
Myles Patricio <myles.patricio@gmail.com>
2026-07-11 00:14:03 -0400

    Add files via upload

---
+++ Python Essential CISCO/Module 2(Data Types,Variables, operatores, Basic IO Operations)/LAB 2.4.7.py
@@ -0,0 +1,27 @@
+# Your task is to:
+#
+#     create the variables: john, mary, and adam;
+#     assign values to the variables. The values must be equal to the numbers of fruit possessed by John, Mary, and Adam respectively;
+#     having stored the numbers in the variables, print the variables on one line, and separate each of them with a comma;
+#     now create a new variable named total_apples equal to the addition of the three previous variables.
+#     print the value stored in total_apples to the console;
+#     experiment with your code: create new variables, assign different values to them,
+#     and perform various arithmetic operations on them (e.g., +, -, *, /, //, etc.).
+#     Try to print a string and an integer together on one line, e.g., "Total number of apples:" and total_apples.
+
+
+john = 1
+
+mary = 6
+
+adam = 3
+
+total_apples = john + mary + adam
+
+
+print(john,mary,adam,sep=",",end="")
+print()
+print("John has:",john)
+print("Mary has:",mary)
+print("Adam has:",adam)
+print("The Total Number of Apples is:",total_apples)
